d9ea0d6d49f39b847b7b0a96304c5d56b0d863ef
kennedymichael54-alt <kennedymichael54@gmail.com>
2025-11-25 13:20:43 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'  // ← Changed this line
+import App from './App.jsx'  // ← Changed this line
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
